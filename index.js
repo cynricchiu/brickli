e05152a567855a98e5836a2323ba8d6902d8349e
cynricchiu <qiuxinyu0609@gmail.com>
2024-05-05 01:04:48 +0800

    vanilla路由修改

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ const actions = {
 				name: 'template',
 				message: chalk.green('请选择工程模板：'),
 				choices: [
-					{ title: 'vanilla', description: '用于基础开发', value: 'vanilla' },
+					{ title: 'vanilla', description: '用于HTML+JS+CSS基础开发', value: 'vanilla' },
+					{ title: 'web component', description: '用于Web Component开发', value: 'web component' },
 					{ title: 'wasm', description: '用于C++&Webassembly开发', value: 'wasm' },
 					{ title: 'react', description: '用于React开发', value: 'react' },
 					{ title: 'react-ts', description: '用于React&TS开发', value: 'react-ts' },
-					{ title: 'webgl', description: '用于WebGL开发', value: 'web component' },
+					{ title: 'webgl', description: '用于WebGL开发', value: 'webgl' },
 				],
 			}).then(response => {
 				const { template } = response;
--- templates/vanilla/pages/index/index.js
+++ templates/vanilla/pages/index/index.js
@@ -22,7 +22,7 @@ function createCardComp(node, ele = document.createDocumentFragment()) {
 		a.className = 'iframe';
 		// a.setAttribute('href', `${href}/index.html`);
 		// a.setAttribute('target', 'demo'); // 绑定到iframe
-		a.setAttribute('route', `${href}/index.html`);
+		a.setAttribute('route', `${href}`);
 		a.innerHTML = name;
 		const span = document.createElement('span');
 		span.className = 'blank';
@@ -54,30 +54,18 @@ document.querySelectorAll('.title').forEach(ele => {
 	});
 });
 // 侧栏点击
+// 页内切换
 document.querySelectorAll('a').forEach(ele => {
 	ele.addEventListener(
 		'click',
 		e => {
-			document.querySelectorAll('a').forEach(e => {
-				e.removeAttribute('selected');
-			});
-			if (!e.target.hasAttribute('selected')) {
-				e.target.setAttribute('selected', '');
-			}
-			// 跳转iframe
 			const route = e.target.getAttribute('route');
-			const url = new URL(route, location.href);
-			iframe.src = url.href;
-			// 对齐theme
-			iframe.onload = e => {
-				const theme = html.getAttribute('theme');
-				e.target.contentDocument.querySelector('html').setAttribute('theme', theme);
-				e.target.contentDocument.querySelector('article')?.setAttribute('data-theme', theme);
-			};
+			history.replaceState({}, null, route);
 		},
 		false
 	);
 });
+// 打开新页面
 document.querySelectorAll('.blank').forEach(ele => {
 	ele.addEventListener(
 		'click',
@@ -89,12 +77,50 @@ document.querySelectorAll('.blank').forEach(ele => {
 		false
 	);
 });
+// 设置主题
 function setTheme(theme = 'light') {
 	html.setAttribute('theme', theme);
 	iframe.contentDocument.querySelector('html').setAttribute('theme', theme);
 	iframe.contentDocument.querySelector('article')?.setAttribute('data-theme', theme);
 }
-setTheme('light');
 document.querySelector('.changeTheme').addEventListener('click', () => {
 	html.getAttribute('theme') === 'light' ? setTheme('dark') : setTheme('light');
 });
+// 路由切换
+function addFuncToListener(targetFunc, name) {
+	return function (...args) {
+		const event = new CustomEvent(name, { bubbles: true, detail: args });
+		const result = targetFunc.apply(this, args);
+		dispatchEvent(event);
+		return result;
+	};
+}
+history.replaceState = addFuncToListener(history.replaceState, 'replaceState');
+window.addEventListener(
+	'replaceState',
+	function (e) {
+		const route = e.detail[2];
+		// 跳转iframe
+		const url = new URL(`${route}/index.html`, location.href);
+		iframe.src = url.href;
+		// 对齐theme
+		iframe.onload = e => {
+			const theme = html.getAttribute('theme');
+			e.target.contentDocument.querySelector('html').setAttribute('theme', theme);
+			e.target.contentDocument.querySelector('article')?.setAttribute('data-theme', theme);
+		};
+		// 侧栏选中
+		document.querySelectorAll('a').forEach(ele => {
+			ele.removeAttribute('selected');
+		});
+		const currentLink = document.querySelector(`[route="${route}"]`);
+		if (currentLink && !currentLink.hasAttribute('selected')) {
+			currentLink.setAttribute('selected', '');
+		}
+	},
+	false
+);
+// 首页加载
+setTheme('light');
+const defaultRoute = window.params.demoList?.children[0]?.href; // 默认路由
+history.replaceState({}, null, defaultRoute);
